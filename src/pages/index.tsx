51e0618dbc1ff04722bd096dcc53a6b48b0f62fe
Godswill Adigwe <gcadigwe@gmail.com>
2023-05-05 14:40:06 +0100

    remove favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <title>Multichainz</title>
         <meta name='description' content='Multichainz Protocol' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        {/* <link rel='icon' href='/favicon.ico' /> */}
       </Head>
 
       <Box h={"100vh"} position={"relative"} bgColor={"#070525"}>
